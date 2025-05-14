b3d0457929be9964009c065a7525fbfbef0c7192
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2025-05-14 17:32:32 +0300

    Add configuration files for Clang tooling

--- boost.sh
+++ boost.sh
@@ -4,3 +4,6 @@ set -xe
 
 rm -rf "${PWD}/.vscode/"
 cp --recursive .devcontainer/vscode/ "${PWD}/.vscode/"
+
+cp .devcontainer/clang/clang-format "${PWD}/.clang-format"
+cp .devcontainer/clang/clang-tidy   "${PWD}/.clang-tidy"
